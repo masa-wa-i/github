56335f03c1da9cf258f2b6f33c3f3e4c5bebb8f5
masa-wa-i <masa.pisces.mark8@gmail.com>
2022-09-27 20:18:20 +0900

    いろいろ

--- ピタゴラス数.py
+++ ピタゴラス数.py
@@ -1,29 +1,18 @@
 import time, json
 def main():
     j = time.time()
-    b = True
-    c = 0
+    count = 100000
     number = []
-    for a in range(100000):
+    for a in range(count):
         number += [a+1]
-    while b:
-        d = number[c]
-        e = True
-        f = []
-        g = 2
-        if not d == 1:
-            while e:
-                f += [d*g]
-                g += 1
-                if d*g > 100000:
-                    e = False
-                    for h in range(len(f)):
-                        i = f[h] in number
-                        if i == True:
-                            del number[number.index(f[h])]
-        c += 1
-        if c > len(number)-1:
-            b = False
+    for m in range(count):
+        d = number[m+1]
+        b = int(count/d)-1
+        for l in range(b):
+            if (d*(l+2) in number) == True:
+                del number[number.index(d*(l+2))]
+        if d > count/2:
+            break
     file = open("エラトステネスの篩.json", "w",encoding="utf-8")
     json.dump(number, file, ensure_ascii=False)
     file.close()
